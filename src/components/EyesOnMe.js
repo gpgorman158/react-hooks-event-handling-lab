5416b6f0ecee3084aadf8fe8dabcea0d7ddb57fc
Gerald Gorman <gpgorman158@gmail.com>
2022-02-24 13:04:10 -0500

    commit tree 1e146a978a7d6b939a5eeefaaa955710edc2ebcf

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe () {
+    
+    function blurCallback (){
+        console.log("Hey! Eyes on me!")
+    }
+    function focusCallback (){
+        console.log("Good!")
+    }
+
+    return(
+        <button onBlur={blurCallback} onFocus={focusCallback}>
+            Eyes on me
+        </button>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+import React from 'react';
+
+function Keypad () {
+    function changeCallback () {
+    console.log("Entering password...");
+    }
+    return(
+        <input type="password" onChange={changeCallback}>
+        
+        </input>
+    );
+}
+
+export default Keypad;
